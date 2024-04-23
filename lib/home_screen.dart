5aa78d3552d1a42030371c7c155b11b8d8a595b3
rane-ganesh <raneganesh888@gmail.com>
2024-04-23 16:26:24 +0530

    Edit and Delete operations done

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:hive/hive.dart';
 import 'package:hive_demo/boxes/boxes.dart';
 import 'package:hive_demo/models/notes_model.dart';
+import 'package:hive_flutter/adapters.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -22,8 +23,61 @@ class _HomeScreenState extends State<HomeScreen> {
           child: Text('Hive Demo'),
         ),
       ),
-      body: const Column(
-        children: [],
+      body: ValueListenableBuilder<Box<NotesModel>>(
+        valueListenable: Boxes.getData().listenable(),
+        builder: (context, box, child) {
+          var data = box.values.toList().cast<NotesModel>();
+          return ListView.builder(
+            itemCount: box.length,
+            itemBuilder: (context, index) {
+              return Card(
+                child: Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Row(
+                        children: [
+                          Text(
+                            data[index].title.toString(),
+                            style: const TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 20,
+                            ),
+                          ),
+                          const Spacer(),
+                          InkWell(
+                            onTap: () {
+                              delete(data[index]);
+                            },
+                            child: const Icon(
+                              Icons.delete,
+                              color: Colors.red,
+                            ),
+                          ),
+                          const SizedBox(
+                            width: 15,
+                          ),
+                          InkWell(
+                            onTap: () {
+                              _editMyDialog(
+                                  data[index],
+                                  data[index].title.toString(),
+                                  data[index].description.toString());
+                            },
+                            child: const Icon(Icons.edit),
+                          )
+                        ],
+                      ),
+                      Text(data[index].description.toString()),
+                    ],
+                  ),
+                ),
+              );
+            },
+          );
+        },
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () async {
@@ -32,6 +86,67 @@ class _HomeScreenState extends State<HomeScreen> {
         child: const Icon(Icons.add),
       ),
     );
+  }
+
+  void delete(NotesModel notesModel) async {
+    await notesModel.delete();
+  }
+
+  Future<void> _editMyDialog(
+      NotesModel notesModel, String title, String description) async {
+    titleController.text = title;
+    descriptionController.text = description;
+
+    return showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            title: const Text('Edit Notes'),
+            content: SingleChildScrollView(
+              child: Column(
+                children: [
+                  TextFormField(
+                    controller: titleController,
+                    decoration: const InputDecoration(
+                      hintText: 'Enter title',
+                      border: OutlineInputBorder(),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  TextFormField(
+                    controller: descriptionController,
+                    decoration: const InputDecoration(
+                      hintText: 'Enter title',
+                      border: OutlineInputBorder(),
+                    ),
+                  )
+                ],
+              ),
+            ),
+            actions: [
+              TextButton(
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  child: const Text('Cancel')),
+              TextButton(
+                  onPressed: () {
+                    notesModel.title = titleController.text.toString();
+                    notesModel.description =
+                        descriptionController.text.toString();
+
+                    notesModel.save();
+                    titleController.clear();
+                    descriptionController.clear();
+
+                    Navigator.pop(context);
+                  },
+                  child: const Text('Edit')),
+            ],
+          );
+        });
   }
 
   Future<void> _showMyDialog() async {
@@ -45,17 +160,17 @@ class _HomeScreenState extends State<HomeScreen> {
                 children: [
                   TextFormField(
                     controller: titleController,
-                    decoration: InputDecoration(
+                    decoration: const InputDecoration(
                       hintText: 'Enter title',
                       border: OutlineInputBorder(),
                     ),
                   ),
-                  SizedBox(
+                  const SizedBox(
                     height: 20,
                   ),
                   TextFormField(
-                    controller: titleController,
-                    decoration: InputDecoration(
+                    controller: descriptionController,
+                    decoration: const InputDecoration(
                       hintText: 'Enter title',
                       border: OutlineInputBorder(),
                     ),
@@ -78,8 +193,6 @@ class _HomeScreenState extends State<HomeScreen> {
                     final box = Boxes.getData();
                     box.add(data);
                     data.save();
-
-                    print(box);
 
                     titleController.clear();
                     descriptionController.clear();
